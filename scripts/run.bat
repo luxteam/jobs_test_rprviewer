81d00e5b5b7762c06137d07b3113ef1c76c8b3fc
LaireZh <zhurbavalery@gmail.com>
2019-08-29 16:55:45 +0300

    gitignore update; run.bat cmd variables implementation

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,4 +1,4 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 
-python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Work\Results --work_dir RprViewer --cmd_variables Tool "..\\RprViewer\\RprViewer.exe" ResPath "C:\\TestResources\\RprViewer"
+python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --file_filter %1 --test_filter "%2" --work_root ..\Work\Results --work_dir RprViewer --cmd_variables Tool "..\\RprViewer\\RprViewer.exe" ResPath "C:\\TestResources\\RprViewer"
