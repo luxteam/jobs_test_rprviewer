689c7997834ddba7de01c7166ac277994c724411
Andrey Alekseevich <magister.demens@gmail.com>
2019-09-28 17:38:27 +0300

    fix stdout parsing for lunix

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -71,9 +71,9 @@ def main():
             main_logger.error("Can't define GPU: {}".format(str(err)))
     elif platform.system() == 'Linux':
         try:
-            s = subprocess.Popen("""clinfo --raw | grep CL_DEVICE_BOARD_NAME | awk '{for(i=3;i<=NF;++i) printf "%s ", $i; print ""}'""", stdout=subprocess.PIPE)
+            s = subprocess.Popen("""clinfo --raw | grep CL_DEVICE_BOARD_NAME | awk '{for(i=3;i<=NF;++i) printf "%s ", $i; print ""}'""", stdout=subprocess.PIPE, shell=True)
             stdout = s.communicate()
-            render_device = stdout[0].decode("utf-8").split('\n')[1].replace('\r', '').strip(' ')
+            render_device = stdout.decode("utf-8").split('\n')[0].replace('\r', '').strip(' ')
         except Exception as err:
             render_device = "undefined_" + platform.uname()[1]
             main_logger.error("Can't define GPU: {}".format(str(err)))
