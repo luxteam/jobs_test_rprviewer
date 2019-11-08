72e4533697dcc0e2d75b3990e421e77106dd661e
LaireZh <zhurbavalery@gmail.com>
2019-11-08 17:17:43 +0300

    update

--- jobs/Scripts/import_tmp.py
+++ jobs/Scripts/import_tmp.py
@@ -5,11 +5,10 @@ import subprocess
 import sys
 from jinja2 import Environment
 from jinja2 import PackageLoader
-#from operator import itemgetter
+
 
 
 def get_jobs_launcher_version(value):
-    # os.chdir(os.path.dirname(__file__))
     return subprocess.check_output("git describe --tags --abbrev=0", shell=True).decode("utf-8")
 
 
@@ -32,7 +31,6 @@ def main():
 	env.filters['get_jobs_launcher_version'] = get_jobs_launcher_version
 	template = env.get_template('base.html')
 
-	#with open("C:\\Users\\user\\RprViewerAuto\\jobs_test_rprviewer\\Work\\summary_report.json", 'r') as file:
 	with open(os.path.join(args.work_dir, "summary_report.json"), 'r') as file:
 		summary_report_data = json.loads(file.read())
 
@@ -56,24 +54,9 @@ def main():
 				except IndexError:
 					graph_data.append([test_case["scene_name"].split('/')[0]])
 					graph_data[test_case_id + 1].append(test_case['render_time_bench'])
-				
-		# graph_data =[ ['Scene name', 'GPU 1', 'GPU 2'],
-		 #        ['New York City, NY', 8175000, 8008000],
-		 #        ['Los Angeles, CA', 3792000, 3694000],
-		 #        ['Chicago, IL', 2695000, 2896000],
-		 #        ['Houston, TX', 2099000, 1953000],
-		 #        ['Philadelphia, PA', 1526000, 1517000] ]
-		 # confs = sorted(summary_report_data, key=lambda conf: conf['Benchmarks'])
 
-		configurations.update({ conf: graph_data })
-		
-	# print(json.dumps(configurations, indent=4))	
-	
-	# print(sorted(configurations))
-	# print(configurations.keys())
-	# print("------")
-	# configurations = {x:configurations[x] for x in sorted(configurations)}
-	
+		configurations.update({ conf: graph_data })		
+
 	with open(os.path.join(args.output_dir, "bench.json"), 'w') as file:
 		json.dump(configurations, file, indent=4)
 
