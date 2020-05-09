93ea6870e8cc036665176eaec84a31450645ed70
GulDilin <zhenyagurin@gmail.com>
2020-05-09 19:48:06 +0300

    add rbs env vars set to run bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,4 +1,12 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
+
+set RBS_BUILD_ID=%3
+set RBS_JOB_ID=%4
+set RBS_URL=%5
+set RBS_ENV_LABEL=%6
+set IMAGE_SERVICE_URL=%7
+set RBS_USE=%8
+
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 
 python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --file_filter %1 --test_filter "%2" --work_root ..\Work\Results --work_dir RprViewer --cmd_variables Tool "..\\RprViewer\\RprViewer.exe" ResPath "C:\\TestResources\\RprViewer"
