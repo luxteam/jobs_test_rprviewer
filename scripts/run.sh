c0c1716d10abf8a48c0084f198e5f79eb7e67036
GulDilin <zhenyagurin@gmail.com>
2020-05-09 22:50:37 +0300

    add export to shell run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-RBS_BUILD_ID=$3
-RBS_JOB_ID=$4
-RBS_URL=$5
-RBS_ENV_LABEL=$6
-IMAGE_SERVICE_URL=$7
-RBS_USE=$8
+export RBS_BUILD_ID=$3
+export RBS_JOB_ID=$4
+export RBS_URL=$5
+export RBS_ENV_LABEL=$6
+export IMAGE_SERVICE_URL=$7
+export RBS_USE=$8
 
 python3 -m pip install -r ../jobs_launcher/install/requirements.txt
 
