3db699f6e8d46ab2357a49f87b0be72c59870205
GulDilin <zhenyagurin@gmail.com>
2020-05-09 20:49:07 +0300

    add rbs envs var set to run sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+RBS_BUILD_ID=$3
+RBS_JOB_ID=$4
+RBS_URL=$5
+RBS_ENV_LABEL=$6
+IMAGE_SERVICE_URL=$7
+RBS_USE=$8
+
 python3 -m pip install -r ../jobs_launcher/install/requirements.txt
 
 python3 ../jobs_launcher/executeTests.py --test_filter $2 --file_filter $1 --tests_root ../jobs --work_root ../Work/Results --work_dir RprViewer --cmd_variables Tool "../RprViewer/RprViewer" ResPath "$CIS_TOOLS/../TestResources/RprViewer"
