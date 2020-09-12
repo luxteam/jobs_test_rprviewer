f6ebdf4f11c37db810023b0d3f49258ad38a53cc
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-09-12 18:49:56 +0300

    fix run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,7 +1,7 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 
-set RETRIES=%1
+set RETRIES=%3
 if not defined RETRIES set RETRIES=2
 
 python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --file_filter %1 --test_filter "%2" --work_root ..\Work\Results --work_dir RprViewer --cmd_variables Tool "..\\RprViewer\\RprViewer.exe" ResPath "C:\\TestResources\\RprViewer" retries %RETRIES%
