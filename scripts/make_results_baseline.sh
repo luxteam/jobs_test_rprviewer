07946d6432a6d31c62a7592c3ec06e3551f284f6
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-09-15 17:36:21 +0300

    add param to make baselines

--- scripts/make_results_baseline.sh
+++ scripts/make_results_baseline.sh
@@ -1,1 +1,4 @@
-python ../jobs_launcher/common/scripts/generate_baselines.py --results_root ../Work/Results/RprViewer --baseline_root ../Work/Baseline
+#!/bin/bash
+DELETE_BASELINES=${1:False}
+
+python ../jobs_launcher/common/scripts/generate_baselines.py --results_root ../Work/Results/Blender28 --baseline_root ../Work/Baseline --remove_old $DELETE_BASELINES
